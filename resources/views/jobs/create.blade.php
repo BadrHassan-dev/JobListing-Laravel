ef9d32d3d1df213f8c0589b9f8180ce582ec503d
badr <badrhassan.w@gmail.com>
2025-03-19 03:58:59 +0200

    Laravel listing jobs

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -21,17 +21,15 @@
                         @enderror
                     </div>
                 </div>
-                <div class="sm:col-span-4">
-                    <label for="salary" class="block text-sm/6 font-medium text-gray-900">Salary</label>
+
+                <x-form-field>
+                    <x-form-label for="salary">Salary</x-form-label>
                     <div class="mt-2">
-                        <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                            <input type="text" name="salary" id="salary" class="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" placeholder="100000" required>
-                        </div>
-                        @error('salary')
-                            <p class="mt-2 text-sm/6 text-red-600">{{ $message }}</p>
-                        @enderror
+                        <x-form-input type="text" name="salary" id="salary"  placeholder="100000" required />
+                        <x-form-error name="salary" />
                     </div>
-                </div>
+                </x-form-field>
+
                 <div class="col-span-full">
                     <label for="description" class="block text-sm/6 font-medium text-gray-900">Descreption</label>
                     <div class="mt-2">
@@ -48,7 +46,7 @@
 
     <div class="mt-6 flex items-center justify-end gap-x-6">
         <button type="button" class="text-sm/6 font-semibold text-gray-900">Cancel</button>
-        <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+        <x-form-button type="submit">Save</x-form-button>
     </div>
 </form>
 
--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,21 @@
 use Illuminate\Support\Facades\Route;
 use App\Models\Job;
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\RegisterUserController;
+use App\Http\Controllers\SessionController;
 
 // Home page
 Route::view('/', 'home');
 
 // Contact page
 Route::view('/contact', 'contact');
+
+// Register page
+Route::get('/register', [RegisterUserController::class, 'create']);
+Route::post('/register', [RegisterUserController::class, 'store']);
+
+Route::get('/login', [SessionController::class, 'create']);
+Route::post('/login', [SessionController::class, 'store']);
 
 // Job routes - JobController is a resource controller
 // Functions in JobController are: index, show, create, store, edit, update, destroy
